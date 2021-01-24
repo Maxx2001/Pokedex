dc4bce1281cbea75b3450d50c69676794856a7dd
Maxx2001 <max.felis11@gmail.com>
2021-01-24 15:11:44 +0100

    Tailwind setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-center text-6xl mt-4'>
+                Pokedex
+            </h1>
+        </div>
+    )
+}
+
+export default Header
